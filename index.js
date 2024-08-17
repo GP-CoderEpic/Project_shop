8b71180e7214631b556503c21e9a955f9b0ba716
GP-CoderEpic <prashargaurav20@gmail.com>
2024-08-17 06:25:46 +0530

    Final progress

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const serverless = require('serverless-http');
+
 const mongoose = require("mongoose");
+
 const { type } = require("os");
 const path = require('path');
+
 const methodOverride = require('method-override');
 require("dotenv").config();
+
+
 const userRouter = require('./routes/user');
 const productRoute = require('./routes/product');
 const app = express();
@@ -18,8 +23,10 @@ mongoose.connect(connecting)
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
+
 app.set('view engine', "ejs");
 app.set('views', path.join(__dirname, 'views'));
+
 app.use('/', userRouter);
 app.use('/product', productRoute);
 
